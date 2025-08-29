4c01810afc1be5ab92523ed2074546503304bf48
PC_Principal <prisci_redon29@hotmail.com>
2025-08-29 15:34:01 -0300

    Empezando con herencia

---
+++ poo_clase_04/src/ar/com/educacionit/poo_clase_04/Contacto.java
@@ -0,0 +1,54 @@
+package ar.com.educacionit.poo_clase_04;
+//Superclase
+public class Contacto {
+
+	//Atributos
+	private String nombre;
+	private String correo;
+	private Direccion direccion;
+	
+	
+	//Constructor por defecto
+		
+	//Constructor parametrizado
+	public Contacto(String nombre, String correo, Direccion direccion) {
+		super();
+		this.nombre = nombre;
+		this.correo = correo;
+		this.direccion = direccion;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getCorreo() {
+		return correo;
+	}
+
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
+
+	public Direccion getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(Direccion direccion) {
+		this.direccion = direccion;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+}
+
+
--- poo_clase_04/src/ar/com/educacionit/poo_clase_04/ContactoProfesional.java
+++ poo_clase_04/src/ar/com/educacionit/poo_clase_04/ContactoProfesional.java
@@ -1,5 +1,27 @@
 package ar.com.educacionit.poo_clase_04;
 
-public class ContactoProfesional {
+public class ContactoProfesional extends Contacto{
+	
+	private String empresa;
+
+	
+	
+	
+	public ContactoProfesional(String nombre, String correo, Direccion direccion, String empresa) {
+		super(nombre, correo, direccion);
+		this.empresa = empresa;
+	}
+
+	public String getEmpresa() {
+		return empresa;
+	}
+
+	public void setEmpresa(String empresa) {
+		this.empresa = empresa;
+	}
+	
+	
+	
+	
 
 }

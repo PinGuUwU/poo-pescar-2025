ac9a6d51954cbdb4778c201343612d9e2d44c321
PC_Principal <prisci_redon29@hotmail.com>
2025-08-29 16:18:47 -0300

    Testeamos tanto el ContactoProfesional como el ContactoPersonal

--- poo_clase_04/src/ar/com/educacionit/poo_clase_04/Test.java
+++ poo_clase_04/src/ar/com/educacionit/poo_clase_04/Test.java
@@ -6,12 +6,17 @@ public class Test {
 		// TODO Auto-generated method stub
 		System.out.println("Hola mundillo!");
 		
-		Direccion d1 = new Direccion("Juan", 1234, 1234);
+		Direccion dpersonal = new Direccion("Juan", 1234, 1234);
 		
 		
-		ContactoPersonal cpersonal = new ContactoPersonal("Juan", "Juan@gmail,com", d1, "29/09/2000");
+		ContactoPersonal cpersonal = new ContactoPersonal("Juan", "Juan@gmail,com", dpersonal, "29/09/2000");
 		
 		System.out.println(cpersonal);
+		
+		Direccion dprofesional = new Direccion("Calle empresa", 555, 1409);
+		ContactoProfesional cprofesional = new ContactoProfesional("Laura", "Laura@gmail.com", dprofesional, "ACME");
+		
+		System.out.println(cprofesional);
 		
 	}
 

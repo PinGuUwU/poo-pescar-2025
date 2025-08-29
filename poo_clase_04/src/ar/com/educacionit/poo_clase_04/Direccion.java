f5a0ce61b6500d08567b387963ae764eba60aa5f
PC_Principal <prisci_redon29@hotmail.com>
2025-08-29 15:05:52 -0300

    Nuevas clases

---
+++ poo_clase_04/src/ar/com/educacionit/poo_clase_04/Direccion.java
@@ -0,0 +1,52 @@
+package ar.com.educacionit.poo_clase_04;
+
+public class Direccion {
+
+	//Atributos
+	private String nombre;
+	private int altura;
+	private int codigoPostal;
+	
+	//Métodos
+	
+	//Getters y Setters
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public int getAltura() {
+		return altura;
+	}
+	public void setAltura(int altura) {
+		this.altura = altura;
+	}
+	public int getCodigoPostal() {
+		return codigoPostal;
+	}
+	public void setCodigoPostal(int codigoPostal) {
+		this.codigoPostal = codigoPostal;
+	}
+	
+	//Constructor por defecto
+	public Direccion() { }
+
+	
+	//Constructor parametrizado
+	public Direccion(String nombre, int altura, int codigoPostal) {
+		super();
+		this.nombre = nombre;
+		this.altura = altura;
+		this.codigoPostal = codigoPostal;
+	}
+
+	//toString
+	@Override
+	public String toString() {
+		return "Direccion [nombre=" + nombre + ", altura=" + altura + ", codigoPostal=" + codigoPostal + "]";
+	}
+	
+	
+	
+}
